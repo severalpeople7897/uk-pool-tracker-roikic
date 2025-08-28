7d445c2146483dda3e9fd20bc79fcbde25a5f021
FrithFudge <53236291+severalpeople7897@users.noreply.github.com>
2025-08-28 12:19:50 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
 import 'expo-router/entry';
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/uk-pool-tracker-roikic/sw.js");
+  });
+}
